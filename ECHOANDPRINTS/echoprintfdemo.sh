148cc499c8eb48c0ac26dd83e2f1ab9fbef176ae
Bhadra <bhadra9821@gmail.com>
2024-08-18 10:37:00 +0530

    Echo and Printf scripts added

---
+++ ECHOANDPRINTS/echoprintfdemo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Write a script using printf command and print text lines.
+
+echo "Hello World!"
+printf "Name:%s\nAge:%d\n" "Veera" 29
+
+# Write a script using echo print multiple lines one by one.
+
+echo -e "This is sajja veerabhadraiah.\nIam a become a DevOps engineer.\nWelcome to world!"
+
+# Write a script that print a variable within a script using printf.
+
+name="veera"
+
+printf "Hello,%s" "$name"
---
+++ ECHOANDPRINTS/printfecho.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Write a script that using echo print single line.
+
+echo -n "This is a normal,consider for ever."
+
+# Write a script that using printf and the floating-point number with two decimal places.
+
+printf "\nValue:%.2f\n" 20
+
+echo "Normal situation" > output.txt
